5b04c2929002f708d93e62b0cb0707d3cfec481e
Kyungdahm Yun <tomyun@gmail.com>
2022-04-23 16:57:28 -0700

    Use step plot for leaf count

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,7 +11,7 @@ import Dates
         snap=s -> Dates.hour(s.calendar.time') == 12,
     )
     @test r.leaves_initiated[end] > 0
-    visualize(r, :DAP, [:leaves_appeared, :leaves_mature, :leaves_dropped]) |> display # Fig. 3.D
+    visualize(r, :DAP, [:leaves_appeared, :leaves_mature, :leaves_dropped], kind=:step) |> display # Fig. 3.D
     visualize(r, :DAP, :green_leaf_area) |> display # Fig. 4.D
     visualize(r, :DAP, [:leaf_mass, :bulb_mass, :total_mass]) |> display
 end
